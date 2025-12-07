cef9524ef9c6259334de65ae474481af11bb0972
kiwon <kkw-11@naver.com>
2025-12-07 13:58:25 +0900

    first commit

---
+++ src/main/java/com/shop/rest/RestShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestShopApplication.class, args);
+	}
+
+}
