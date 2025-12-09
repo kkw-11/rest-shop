127e39f5c20052432f074006b667ec4748b715c5
kiwon <kkw-11@naver.com>
2025-12-09 21:59:24 +0900

    feat:#1 JPA Repository Interface

---
+++ src/main/java/com/shop/rest/commerce/repository/ItemRepository.java
@@ -0,0 +1,17 @@
+package com.shop.rest.commerce.repository;
+
+import com.shop.rest.commerce.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+}
