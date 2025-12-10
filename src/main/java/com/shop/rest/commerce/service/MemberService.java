04499c8e71125fce2fc700c7830fc16b84258077
kiwon <kkw-11@naver.com>
2025-12-10 21:58:04 +0900

    feat:#2 MemberService, saveMember, validateDuplicateMember, loadUserByUsername

---
+++ src/main/java/com/shop/rest/commerce/service/MemberService.java
@@ -0,0 +1,49 @@
+package com.shop.rest.commerce.service;
+
+import com.shop.rest.commerce.entity.Member;
+import com.shop.rest.commerce.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class MemberService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member){
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member){
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null){
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null) {
+            throw new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다: " + email);
+        }
+
+        log.info("로그인 사용자: {}", email);
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
+    }
+}
