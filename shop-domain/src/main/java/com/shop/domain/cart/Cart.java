833dade84c88454d50c2f5f83c0b91d4d73fd3d1
kiwon <kkw-11@naver.com>
2025-12-12 22:25:43 +0900

    refactor:#8 Cart Entity 모듈이동

--- shop-domain/src/main/java/com/shop/domain/cart/Cart.java
+++ shop-domain/src/main/java/com/shop/domain/cart/Cart.java
@@ -1,5 +1,7 @@
-package com.shop.rest.commerce.entity;
+package com.shop.domain.cart;
 
+import com.shop.domain.common.BaseEntity;
+import com.shop.domain.member.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
