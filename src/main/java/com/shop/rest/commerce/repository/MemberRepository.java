582a3164bac838b0cdbaa4ab054651176b230925
kiwon <kkw-11@naver.com>
2025-12-12 12:10:58 +0900

    feat:#2 Email 기반 회원 조회 Repository

--- src/main/java/com/shop/rest/commerce/repository/MemberRepository.java
+++ src/main/java/com/shop/rest/commerce/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package com.shop.rest.commerce.repository;
 import com.shop.rest.commerce.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
