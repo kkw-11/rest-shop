7f453d900a4f13f2677aca0e3c93cef4fdf52dbf
kiwon <kkw-11@naver.com>
2025-12-12 10:40:47 +0900

    feat:#2 회원 등록, 회원 조회 API

--- src/main/java/com/shop/rest/commerce/constant/ResponseMessage.java
+++ src/main/java/com/shop/rest/commerce/constant/ResponseMessage.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ResponseMessage {
+    SUCCESS_MESSAGE("요청이 성공했습니다."),
+
     MEMBER_REGISTERED("회원가입이 완료되었습니다."),
     MEMBER_UPDATED("회원 정보가 수정되었습니다.");
 
