949373be05f3ffd6a3fc13c30100e80893440b5e
kiwon <kkw-11@naver.com>
2025-12-12 22:27:15 +0900

    refactor:#8 MemberRepository 모듈이동

--- shop-domain/src/main/java/com/shop/domain/member/MemberRepository.java
+++ shop-domain/src/main/java/com/shop/domain/member/MemberRepository.java
@@ -1,12 +1,14 @@
-package com.shop.rest.commerce.repository;
+package com.shop.domain.member;
 
-import com.shop.rest.commerce.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    List<Member> findAll();
 }
