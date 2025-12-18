4102808b7fb032335f0fa2e13d779eced4588997
kiwon <kkw-11@naver.com>
2025-12-19 07:52:28 +0900

    feat:#2 RefreshTokenRepository

---
+++ shop-domain/src/main/java/com/shop/domain/auth/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.shop.domain.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByMemberId(Long memberId);
+    void deleteByMemberId(Long memberId);
+
+}
