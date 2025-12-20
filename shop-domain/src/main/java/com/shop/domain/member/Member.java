1d5366deec865949e0607ab2ae27b0c1cd8cb49a
kiwon <kkw-11@naver.com>
2025-12-20 17:11:04 +0900

    feat:#2  Member Entity 회원 생성 로직 추가

--- shop-domain/src/main/java/com/shop/domain/member/Member.java
+++ shop-domain/src/main/java/com/shop/domain/member/Member.java
@@ -3,13 +3,15 @@ package com.shop.domain.member;
 import com.shop.common.constant.Role;
 import com.shop.domain.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
-@Table(name="member")
-@Getter @Setter
+@Table(name = "member")
+@Getter
+@NoArgsConstructor(access =  AccessLevel.PROTECTED)
 @ToString
 public class Member extends BaseEntity {
     @Id
@@ -29,4 +31,27 @@ public class Member extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    /**
+     * 정적 팩토리 메서드 - 신규회원 생성
+     * @param email
+     * @param encodedPassword
+     * @param name
+     * @param address
+     * @return
+     */
+
+    public static Member createMember(
+            String email,
+            String encodedPassword,
+            String name,
+            String address
+    ) {
+        Member member = new Member();
+        member.email = email;
+        member.password = encodedPassword;
+        member.name = name;
+        member.address = address;
+        member.role = Role.USER; //비즈니스 규칙 신규회원은 기본적으로 USER 권한
+        return member;
+    }
 }
