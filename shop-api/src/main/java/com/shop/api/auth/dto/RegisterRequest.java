8fc860c3ced16123185e7d87a8bf0b9320c6f229
kiwon <kkw-11@naver.com>
2025-12-21 12:16:12 +0900

    feat:#2 RegisterRequest 매퍼메서드

--- shop-api/src/main/java/com/shop/api/auth/dto/RegisterRequest.java
+++ shop-api/src/main/java/com/shop/api/auth/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.shop.api.auth.dto;
 
+import com.shop.core.member.dto.CreateMemberCommand;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,4 +14,13 @@ public class RegisterRequest {
     private String password;
     private String name;
     private String address;
+
+    public CreateMemberCommand toCommand() {
+        return CreateMemberCommand.builder()
+                .email(this.email)
+                .password(this.password)
+                .name(this.name)
+                .address(this.address)
+                .build();
+    }
 }
