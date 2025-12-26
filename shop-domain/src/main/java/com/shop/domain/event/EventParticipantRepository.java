bca57d3e949f0c5542e6b371924b96434508c2cc
kiwon <kkw-11@naver.com>
2025-12-26 13:34:17 +0900

    feat:#17 이벤트 참여 횟수 쿼리메서드 countByEventId

--- shop-domain/src/main/java/com/shop/domain/event/EventParticipantRepository.java
+++ shop-domain/src/main/java/com/shop/domain/event/EventParticipantRepository.java
@@ -10,9 +10,12 @@ public interface EventParticipantRepository extends JpaRepository<EventParticipa
      * 특정 회원이 해당 이벤트에 이미 참여했는지 확인
      */
     @Query("SELECT COUNT(ep) > 0 FROM EventParticipant ep " +
-            "WHERE ep.event.id = :eventId AND ep.member.id = :memberId")
+            "WHERE ep.event.id = :eventId AND ep.member.memberId = :memberId")
     boolean existsByEventIdAndMemberId(
             @Param("eventId") Long eventId,
             @Param("memberId") Long memberId
     );
+
+    // COUNT 쿼리
+    long countByEventId(Long eventId);
 }
